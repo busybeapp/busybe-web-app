092125c327a027c33fbe0dc2fa6ea709d5682d23
web-deploy <web-deploy@github.com>
2025-03-05 19:34:38 +0000

    fix: enable network client to macos (#46)
    Co-authored-by: Amazia Gur <amaziagur@Amazias-MacBook-Pro.local>

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "manifest.json": "5b2959f3e6055508c4ea193d456b2139",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "a548df148fdb625ce9c27d286dcb8231"};
+"flutter_bootstrap.js": "463c17d4810138918b0bf27be83b7ad7"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
